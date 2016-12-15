0c886073b62b4a881f93133b1e82ab5828e899d7
Anja Kefala <anja.kefala@gmail.com>
2016-12-15 11:18:57 -0500

    Refined tree class.

--- Lisp.py
+++ Lisp.py
@@ -5,6 +5,9 @@
 #During your interview, you will pair on writing an interpreter to run the AST. You can start by implementing a single built-in function (for example, +) and add more if you have time.
 
 class Tree:
+    """
+    A generic tree build.
+    """
     def __init__(self, value, children=None):
         self.children = []
         self.value = value
@@ -14,10 +17,10 @@ class Tree:
         for child in children:
             if not isinstance(child, Tree):
                 print("Error: child is not an object of type Tree")
-                return(-1)
+                return(0)
             else:
                 self.children.append(child)
-        return(0)
+        return(1)
     def __str__(self, level=0):
         tree = "\t"*level + str(self.value) + "\n"
         if self.children is not None:
@@ -25,8 +28,10 @@ class Tree:
                 tree += child.__str__(level+1)
         return(tree)
 
-testTree = Tree(2)
+tree1 = Tree("+", [Tree(2), Tree(3)])
+tree2 = Tree("list", [Tree(1), tree1, Tree(9)])
+tree3 = Tree("first", [tree2])
 
-testTree .add_children([Tree(2), Tree(3), Tree(4)])
 
-print(testTree)
+
+print(tree3)
